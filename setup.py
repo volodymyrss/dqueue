63aad1045e9b8ec14ce62d9b6e742916038d5de9
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2018-07-07 22:14:43 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+setup(name='dqueue',
+      version='1.0',
+      description='a queue manager (yet another)',
+      author='Volodymyr Savchenko',
+      author_email='vladimir.savchenko@gmail.com',
+      py_modules=['dqueue'],
+     )
