fbbf495d81b619dde2d131c232abd3e4f4eb1622
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-06-26 22:05:12 +0200

    basepath for api

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,6 @@ ADD static /static
 ADD dist/* /dist/
 RUN pip install /dist/*
 
+ENV API_BASE=/staging-1-3/dqueue
 
 ENTRYPOINT gunicorn dqueue.api:app -b 0.0.0.0:8000 --log-level DEBUG
--- dqueue/api.py
+++ dqueue/api.py
@@ -27,7 +27,33 @@ decoded_entries={} # type: ignore
 db = dqueue.core.db
 
 app = dqueue.app.app
-swagger = Swagger(app)
+
+
+template = {
+  "swagger": "2.0",
+#  "info": {
+ #   "title": "My API",
+#    "description": "API for my data",
+    #"contact": {
+    #  "responsibleOrganization": "ME",
+    #  "responsibleDeveloper": "Me",
+    #  "email": "me@me.com",
+    #  "url": "www.me.com",
+    #},
+    #"termsOfService": "http://me.com/terms",
+    #"version": "0.0.1"
+ # },
+  #"host": "mysite.com",  # overrides localhost:500
+  "basePath": os.environ.get("API_BASE", "/"),  # base bash for blueprint registration
+  "schemes": [
+    "http",
+    "https"
+  ],
+ # "operationId": "getmyData"
+}
+
+
+swagger = Swagger(app, template=template)
 
 print("setting up app", app, id(app))
 
