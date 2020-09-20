bff5c7616f7ba33adebafde5bcc931ea1c921025
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-09-20 18:58:21 +0200

    a bit more renamed

--- dqueue/api.py
+++ dqueue/api.py
@@ -358,7 +358,7 @@ class WorkerDataConsultFact(SwaggerView):
         logger.info("worker %s consulting fact of dag %s", worker_id, len(dag))
 
         dag_bucket = "odahub-" + odakb.datalake.form_bucket_name(dag)
-        logger.info("dag bucket %s", dag_bucket)
+        logger.info("dag %s bucket %s", repr(dag)[:30], dag_bucket)
 
         try:
             meta, payload  = odakb.datalake.restore(dag_bucket, return_metadata=True)
