88633f3c279066bc5f71905285745298186a209a
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-09-23 23:34:37 +0200

    Bump version: 1.1.26-dev0 → 1.1.26

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dda-queue',
-      version='1.1.26-dev0',
+      version='1.1.26',
       description='a queue manager (yet another)',
       author='Volodymyr Savchenko',
       author_email='vladimir.savchenko@gmail.com',
