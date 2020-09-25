54953323e31e285e551f2e7ac75dac8034604e23
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-09-25 12:49:33 +0200

    from_uri accepts no parameters

--- dqueue/__init__.py
+++ dqueue/__init__.py
@@ -5,11 +5,16 @@ from .data import DataFacts
 
 import logging
 
-def from_uri(queue_uri: str):
+def from_uri(queue_uri: Union[str, None]=None):
     """"""
     logger = logging.getLogger("from_uri")
 
     local, remote = Queue, QueueProxy
+
+    if queue_uri is None:
+        if 'ODAHUB' in os.environ:
+            queue_uri = os.environ['ODAHUB']
+            logger.info("getting ODAHUB from env: %s", queue_uri)
 
     if queue_uri.startswith("http://") or queue_uri.startswith("https://"):
         r = remote(queue_uri)
