ee51a000769bdab35fc9d7187b79f71e5574608c
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-10-07 09:26:35 +0200

    longer guardian steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ echo "APP_MODE: ${APP_MODE:=api}"
 if [ ${APP_MODE:?} == "api" ]; then
     gunicorn --workers 8 dqueue.api:app -b 0.0.0.0:8000 --log-level DEBUG
 elif [ ${APP_MODE:?} == "guardian" ]; then
-    dqueue guardian -w 10
+    dqueue guardian -w 30
 else
     echo 'unknown APP_MODE! can be "api" or "guardian"'
     exit 1
