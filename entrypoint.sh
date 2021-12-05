2dddcdb3fcaf772beaec3d409577ab579c0be522
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-12-05 09:33:03 +0100

    more log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 echo "APP_MODE: ${APP_MODE:=api}"
 
 if [ ${APP_MODE:?} == "api" ]; then
-    gunicorn --workers 8 dqueue.api:app -b 0.0.0.0:8000 --log-level ${DQUEUE_LOG_LEVEL:-WARNING} --timeout 600 2>&1 | grep -v DEBUG | grep -v INFO | cut -c1-500
+    gunicorn --workers 8 dqueue.api:app -b 0.0.0.0:8000 --timeout 600 --log-level DEBUG 2>&1 
+    #gunicorn --workers 8 dqueue.api:app -b 0.0.0.0:8000 --log-level ${DQUEUE_LOG_LEVEL:-WARNING} --timeout 600 2>&1 | grep -v DEBUG | grep -v INFO | cut -c1-500
 elif [ ${APP_MODE:?} == "guardian" ]; then
     while true; do
         dqueue guardian -w 30
